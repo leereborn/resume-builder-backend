224b195f42aeff5faece072dac2bb7f05c791ed2
Rui Li <leereborn92929@gmail.com>
2021-06-07 12:51:55 -0400

    Update database config

--- models/Database.js
+++ models/Database.js
@@ -1,10 +1,21 @@
 require('dotenv').config();
 const AWS = require("aws-sdk");
 
-AWS.config.update({
-    region: process.env.DB_REGION,
-    endpoint: process.env.DB_ENDPOINT
-});
+let db_config;
+if (process.env.DB_LOCAL==='true'){
+    db_config = {
+        region: process.env.DB_REGION,
+        endpoint: process.env.DB_ENDPOINT
+    }
+}else{//remote
+    db_config = {
+        region: process.env.DB_REGION,
+        accessKeyId: process.env.DB_KEY_ID,
+        secretAccessKey: process.env.DB_KEY
+    }
+}
+AWS.config.update(db_config);
+
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 const db_get = async (params)=>{
